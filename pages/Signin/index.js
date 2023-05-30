5715043829ccb5e42050939c03aedbc8a874e6bc
Gabriel <gabrielmourarocha@gmail.com>
2023-05-30 11:16:08 -0300

    login- condicional/sem tela add

--- pages/Signin/index.js
+++ pages/Signin/index.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import { KeyboardView , Container,  Title , Input , ButtonSubmit , TextSubmit} from './style';
+import React, { useState } from 'react';
+import { KeyboardView, Container, Title, Input, ButtonSubmit, TextSubmit } from './style';
 import Header from '../../components/Header';
 
-
-
-
-
 function Signin() {
-    return(
-        <KeyboardView>
-            <Header />
-            <Container> 
-                <Title>Login</Title>
-                
-                <Input placeholder="Usuário"/>
+  const [isLoginValidated, setIsLoginValidated] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-                <Input placeholder="Senha" secureTextEntry/>
-                <ButtonSubmit>
-                   <TextSubmit>Entrar</TextSubmit>
-                </ButtonSubmit>
-            </Container>
-        </KeyboardView>
+  const handleLogin = () => {
+    // Valores esperados para usuário e senha
+    const useresperado = 'zenha';
+    const passesperado = 'adminz';
 
+   
+    if (username === useresperado && password === passesperado) {
+     
+      window.alert('Bem-vindo: Admin');
+      
+    } else {
+      
+      window.alert('Usuário e/ou Senha Inválido(s).');
+    }
+  };
 
-    )
+  return (
+    <KeyboardView>
+      <Header />
+      <Container>
+        <Title>Login</Title>
+
+        <Input
+          name="username"
+          placeholder="Usuário"
+          value={username}
+          onChangeText={(text) => setUsername(text)}
+        />
+
+        <Input
+          name="password"
+          placeholder="Senha"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <ButtonSubmit onPress={handleLogin}>
+          <TextSubmit>Entrar</TextSubmit>
+        </ButtonSubmit>
+      </Container>
+    </KeyboardView>
+  );
 }
 
 export default Signin;
